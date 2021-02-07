8c6adcff713fec91571deebed5fd11e1698300d4
CeylonP <ceylonp20@gmail.com>
2021-02-08 02:45:30 +0900

    Add templates html css And Config Setting

---
+++ src/main/java/com/nalssam/web/dto/IndexController.java
@@ -0,0 +1,13 @@
+package com.nalssam.web.dto;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "maindesktop.html";
+    }
+}
